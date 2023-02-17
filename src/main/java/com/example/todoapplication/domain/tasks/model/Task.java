dbe291aff2f31d4baddedf479d5b1e25d0eee26d
aneta.arsova <anetaarsova654@yahoo.com>
2023-02-17 09:32:23 +0100

    lombok annotations added

--- src/main/java/com/example/todoapplication/domain/tasks/model/Task.java
+++ src/main/java/com/example/todoapplication/domain/tasks/model/Task.java
@@ -2,12 +2,20 @@ package com.example.todoapplication.domain.tasks.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.io.Serializable;
 
 @Entity
 @Table
 @JsonIgnoreProperties({"hibernateLazyInitializer"})
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
 public class Task implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,18 +26,23 @@ public class Task implements Serializable {
     private String content;
     @Column
     private boolean completed;
+
+    public Task() {
+
+    }
+
     public void setId(Long id) {
         this.id = id;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    @Override
-    public String toString() {
-        return "Task{" +
-                "id=" + id +
-                '}';
-    }
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Task{" +
+//                "id=" + id +
+//                '}';
+//    }
 }
